75c0b869227b21ec792dacede273f462134cedc7
Dmitriy <dmitriytuz123@gmail.com>
2024-04-01 15:54:50 +0300

    add parameter isDatesInMs for getAllTasks

--- src/entities/task/task.module.ts
+++ src/entities/task/task.module.ts
@@ -5,11 +5,12 @@ import { TaskController } from '@src/entities/task/task.controller';
 import { UserModule } from '@src/entities/user/user.module';
 import { Task } from '@src/entities/task/task.entity';
 import { User } from '@src/entities/user/user.entity';
+import {HelperModule} from "@src/helper/helper.module";
 
 @Module({
   controllers: [TaskController],
   providers: [TaskService],
-  imports: [TypeOrmModule.forFeature([Task, User]), UserModule],
+  imports: [TypeOrmModule.forFeature([Task, User]), UserModule, HelperModule],
   exports: [TaskService],
 })
 export class TaskModule {}
