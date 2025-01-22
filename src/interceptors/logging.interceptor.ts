355751cf1b048f5a8e5ee63637954dea231b3818
Dmitriy <dmitriytuz123@gmail.com>
2025-01-22 12:11:47 +0200

    refactoring 1

--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -1,7 +1,7 @@
 import {
   CallHandler,
   ExecutionContext,
-  Injectable,
+  Injectable, Logger,
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
@@ -9,16 +9,16 @@ import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(LoggingInterceptor.name);
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const now = Date.now();
 
-    console.log('Request processed:', context.switchToHttp().getRequest().url);
+    this.logger.log(`Request processed: ${context.switchToHttp().getRequest().url}`);
 
     return next.handle().pipe(
       tap(() =>
-        console.log(
-          `Request completed. Execution time: ${Date.now() - now}ms`,
-        ),
+          this.logger.log(`Request completed. Execution time: ${Date.now() - now}ms`)
       ),
     );
   }
